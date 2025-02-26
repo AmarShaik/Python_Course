b712b92f7892bbec195a895c96ae901bc1eaecce
Amar <shaikama907@gmail.com>
2025-02-26 11:26:33 +0530

    typecasting lesson Python_Course project

--- 2 - typecasting.py
+++ 2 - typecasting.py
@@ -1,2 +1,23 @@
 # typecasting - The process of converting a variable from one dat atype to another
 #               str(), int(), float(), bool()
+
+name = "Stephen"
+age = 25
+gpa = 8.8
+is_student = True
+
+gpa = int(gpa)
+print(gpa)              # 8
+
+age = str(age)
+age += "1"              #251
+# age = float(age)
+print(age)              # 25.0
+
+# age = str(age)
+# print(age)
+# print(type(age))       #<class 'str'>
+
+name = bool(name)
+print(name)             #True
+
