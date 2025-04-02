cd2431205ed4a329b49010750523d4424d190426
Amar <shaikama907@gmail.com>
2025-04-02 23:57:29 +0530

    inputs lesson Python_Course project

---
+++ 7 - if-else statements.py
@@ -0,0 +1,44 @@
+'''if-else Statements
+Sometimes the programmer needs to check the evaluation of certain expression(s), whether the expression(s) evaluate to True or False. If the expression evaluates to False, then the program execution follows a different path than it would have if the expression had evaluated to True.
+
+Based on this, the conditional statements are further classified into following types:
+
+if
+if-else
+if-else-elif
+nested if-else-elif'''
+
+# 1 - if-else
+applePrice = 10
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+
+# 2 - if-elif-else
+num = int(input("Enter the value of num: "))
+if (num < 0):
+  print("Number is negative.")
+elif (num == 0):
+  print("Number is Zero.")
+elif (num == 999):
+  print("Number is Special.")
+else:
+  print("Number is positive.")
+
+print("I am happy now")
+
+# 3 - nested
+num = 18
+if (num < 0):
+    print("Number is negative.")
+elif (num > 0):
+    if (num <= 10):
+        print("Number is between 1-10")
+    elif (num > 10 and num <= 20):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
