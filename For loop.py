2cef527035edbe3c1fdb349832ad4ff4cf87869f
Amar <shaikama907@gmail.com>
2025-04-20 22:46:59 +0530

    inputs lesson Python_Course project

---
+++ For loop.py
@@ -0,0 +1,33 @@
+#In Python, a for loop is used to iterate over a sequence
+# (such as a list, tuple, dictionary, set, or string) and execute a block of code for each item in the sequence.
+# syntax = for variable in iterable:
+                #rest of the code
+#example - fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+# output:
+# apple
+# banana
+# cherry
+
+# name = 'Abhishek'
+# for i in name:
+#   print(i)
+#   if(i =="b"):
+#     print("This is something special!")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+# for k in range(5):
+#   print(k + 1)
+
+# for k in range(1, 20001):
+#   print(k)
+
+
+for k in range(1, 12, 3):
+    print(k)
